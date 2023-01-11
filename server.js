67595cfed7a56a9825d73507bf6506fdaa3723fb
Eric K <erickirberger@gmail.com>
2023-01-11 17:26:35 -0500

    added more boilerplate folders, files, and code. go through requirements and starts undertsanding how you want to build this code

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/dish-routes'));
 
 const sess = {
   secret: 'Super secret secret',
@@ -26,5 +35,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`App listening on PORT ${PORT} 🚀`));
 });
